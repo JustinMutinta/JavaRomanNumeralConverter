eb76516cac99eaaaf1cd4758366f85be36673d97
JustinMutinta <justinmutinta@hotmail.com>
2021-09-01 11:43:38 -0500

    2nd pass. Created classes and used Polymorphism, Override and Overload

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Vehicle honda = new Vehicle();
+
+        honda.sound();
+        honda.size(5, 3);
+
+        Import benz = new Import();
+
+        benz.sound();
+        benz.size(5, 3);
+
+        locallyMade ford = new locallyMade();
+        ford.sound();
+        ford.size(5);
+    }
 }
--- Polymorphism/src/Vehicle.java
+++ Polymorphism/src/Vehicle.java
@@ -1,2 +1,24 @@
 public class Vehicle {
+
+    public void size(int height, int width){
+        System.out.println("The vehicle is " + height + " tall and " + width + " wide.");
+    }
+
+    public  void sound(){
+        System.out.println("Vroom");
+    }
+}
+
+class Import extends Vehicle{
+
+    public void sound(){
+        System.out.println("Expensive");
+    }
+}
+
+class locallyMade extends Vehicle{
+
+    public void size(int height){
+        System.out.println("The vehicle is " + height + "tall");
+    }
 }
