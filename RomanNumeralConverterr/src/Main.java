0033ad5b1c830dc0bb414e8d00ec52a916a2c5b9
JustinMutinta <justinmutinta@hotmail.com>
2021-09-03 19:06:13 -0500

    3rd Pass, rough working draft

--- RomanNumeralConverterr/src/Main.java
+++ RomanNumeralConverterr/src/Main.java
@@ -1,8 +1,107 @@
 public class Main {
 
+    public static String oneThousand = "M";
+    public static String nineHundred = "CM";  public static String fiveHundred = "D";   public static String fourHundred = "CD";  public static String oneHundred = "C";
+    public static String ninety = "XC";       public static String fifty = "L";         public static String forty = "XL";        public static String ten = "X";
+    public static String nine = "IX";         public static String five = "V";          public static String four = "IV";         public static String one = "I";
+
     public static void main(String[] args) {
         System.out.println("Test");
+        int testNumber = 8;
+        int testDecidingNumber = 1;
+
+        System.out.println(romanValue(testNumber, testDecidingNumber));
     }
+
+
+    public static String finalCal(int userNumber){
+        String output = "";
+
+
+        return output;
+    }
+
+    public static String romanValue(int userNumber, int decidingnumber){
+        String output = "";
+        int tempNum = 0;
+
+        if(userNumber >= 1000){
+            tempNum = userNumber/decidingnumber;
+            if(tempNum == 1){
+                output += oneThousand;
+            }else{
+                for(int i = 0; i < tempNum; i++){
+                    output += oneThousand;
+                }
+            }
+        }else if(userNumber >= 100){
+            tempNum = userNumber/decidingnumber;
+            if(tempNum == 9){
+                output += nineHundred;
+            }else if(tempNum == 5){
+                output += fiveHundred;
+            }else if(tempNum == 4){
+                output += fourHundred;
+            }else if(tempNum == 1){
+                output += oneHundred;
+            }else if(tempNum > 5 && tempNum < 9){
+                output += fiveHundred;
+                tempNum -= 5;
+                for(int i = 0; i < tempNum; i++){
+                    output += oneHundred;
+                }
+            }else{
+                for(int i = 0; i < tempNum; i++){
+                    output += oneHundred;
+                }
+            }
+        }else if(userNumber >= 10) {
+            tempNum = userNumber / decidingnumber;
+            if (tempNum == 9) {
+                output += ninety;
+            } else if (tempNum == 5) {
+                output += fifty;
+            } else if (tempNum == 4) {
+                output += forty;
+            } else if (tempNum == 1) {
+                output += ten;
+            } else if (tempNum > 5 && tempNum < 9) {
+                output += fifty;
+                tempNum -= 5;
+                for (int i = 0; i < tempNum; i++) {
+                    output += ten;
+                }
+            } else {
+                for (int i = 0; i < tempNum; i++) {
+                    output += ten;
+                }
+            }
+        }else {
+            tempNum = userNumber / decidingnumber;
+            if (tempNum == 9) {
+                output += nine;
+            } else if (tempNum == 5) {
+                output += five;
+            } else if (tempNum == 4) {
+                output += four;
+            } else if (tempNum == 1) {
+                output += one;
+            } else if (tempNum > 5 && tempNum < 9) {
+                output += five;
+                tempNum -= 5;
+                for (int i = 0; i < tempNum; i++) {
+                    output += one;
+                }
+            } else {
+                for (int i = 0; i < tempNum; i++) {
+                    output += one;
+                }
+            }
+        }
+
+        return output;
+    }
+
 }
 
 /*
