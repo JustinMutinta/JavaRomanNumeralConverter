65f9b496a6de3c50d7bd7f285745a3f634f32a6b
MUTINTA <JustinMutinta@hotmail.com>
2021-09-05 22:37:30 -0500

    Final pass. Cleaned code and added comments

--- RomanNumeralConverterr/src/Main.java
+++ RomanNumeralConverterr/src/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
 
-
+    //Create a public 2D Array to hold the values of roman numerals. Everything above 5000 is supposed to have an overline, but since I could not find the
+    //right hexademical code, I found close equivalents to make it work.
     public static String[][] romanArray =   {{"\u1e40"+"\u1e40"+"\u1e40"+"\u1e40"+"\u1e40"+"\u1e40"+"\u1e40"+"\u1e40"+"\u1e40", "\u1e40"+"\u1e40"+"\u1e40"+"\u1e40"+"\u1e40", "\u1e40"+"\u1e40"+"\u1e40"+"\u1e40", "\u1e40"}, //9M, 5M, 4M, 1M
                                             {"\u010a"+"\u1e40", "\u1e0a", "\u010a"+"\u1e0a", "\u010a"},     //900K, 500K, 400K, 100K
                                             {"\u1e8a"+"\u010a", "\u1e38", "\u1e8a"+"\u1e38", "\u1e8a"},    //90,000 , 50,000, 40,000, 10,000
@@ -23,11 +24,12 @@ public class Main {
             System.out.println("Enter another number, or enter '0' to quit:   ");
             testNumber = sc.nextInt();
         }
-
         System.out.println(finalCal(testNumber));
     }
 
 
+    //Uses the romanValue1 to run through each number given.
+    //Starting with 1,000,000 then working down, each value is converted to a roman numeral.
     public static String finalCal(int userNumber){
         String output = "";
         int decidingNumber = 1_000_000;
@@ -37,41 +39,30 @@ public class Main {
             userNumber %= decidingNumber;
             decidingNumber = decidingNumber / 10;
         }
-
         return output;
     }
 
+    //Run through an iteration of calculations. Depending on the size of the userNumber, the relevant array will be used
+    //When running through these, the biggest ones to look out for are 9,5,4, and 1. Everything else is built around these numbers
     public static String romanValue1(int userNumber, int decidingNumber){
         String output = "";
         int array1 = 0;
-        int array2 = 0;
-        if(userNumber >= 1_000_000){
-            array1 = 0;
-        }else if(userNumber >= 100_000){
-            array1 = 1;
-        }else if(userNumber >= 10_000){
-            array1 = 2;
-        }else if(userNumber >= 1000){
-            array1 = 3;
-        }else if(userNumber >= 100){
-            array1 = 4;
-        }else if(userNumber >= 10){
-            array1 = 5;
-        }else{
-            array1 = 6;
-        }
+
+        if(userNumber >= 1_000_000){array1 = 0;}
+        else if(userNumber >= 100_000)  {array1 = 1;}
+        else if(userNumber >= 10_000)   {array1 = 2;}
+        else if(userNumber >= 1000)     {array1 = 3;}
+        else if(userNumber >= 100)      {array1 = 4;}
+        else if(userNumber >= 10)       {array1 = 5;}
+        else                            {array1 = 6;}
 
         int tempNum = 0;
         tempNum = userNumber/decidingNumber;
-        if(tempNum == 9){
-            output += romanArray[array1][0];
-        }else if(tempNum == 5){
-            output += romanArray[array1][1];
-        }else if(tempNum == 4){
-            output += romanArray[array1][2];
-        }else if(tempNum == 1){
-            output += romanArray[array1][3];
-        }else if(tempNum > 5 && tempNum < 9){
+        if(tempNum == 9)        {output += romanArray[array1][0];}
+        else if(tempNum == 5)   {output += romanArray[array1][1];}
+        else if(tempNum == 4)   {output += romanArray[array1][2];}
+        else if(tempNum == 1)   {output += romanArray[array1][3];}
+        else if(tempNum > 5 && tempNum < 9){
             output += romanArray[array1][1];
             tempNum -= 5;
             for(int i = 0; i < tempNum; i++){
